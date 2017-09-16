25c316d672ae1cd2a42488811271426b1db0a242
Juan Pablo Royo <juanpablo.royo@gmail.com>
2017-09-16 17:25:25 +0200

    advance tdd

--- src/test/scala/edu/jproyo/dojos/loan/TestHelper.scala
+++ src/test/scala/edu/jproyo/dojos/loan/TestHelper.scala
@@ -5,12 +5,15 @@ import edu.jproyo.dojos.loan.data.DataLoader
 object TestHelper {
 
   implicit object testDataLoader extends DataLoader {
-    def loadData: Set[Lender] =
-      (for {
-        rate <- (1 to 10).map(0.06 + _.toDouble / 100)
-        amount <- (1 to 10).map(100 * _)
-      } yield Lender("Name "+rate+"-"+amount, rate, amount)).toSet
-
+    def loadData: Set[Lender] = {
+      Set(Lender("Bob", 0.075, 640),
+        Lender("Jane", 0.069, 480),
+        Lender("Fred", 0.071, 520),
+        Lender("Mary", 0.104, 170),
+        Lender("John", 0.081, 320),
+        Lender("Dave", 0.074, 140),
+        Lender("Angela", 0.071, 60))
+    }
   }
 
 }
