967a8711c19869ffcd65640b46a587c85733aa75
Juan Pablo Royo <juanpablo.royo@gmail.com>
2017-09-16 13:42:21 +0200

    advance tdd

--- src/test/scala/edu/jproyo/dojos/loan/LoanSpec.scala
+++ src/test/scala/edu/jproyo/dojos/loan/LoanSpec.scala
@@ -9,7 +9,9 @@ class LoanSpec extends WordSpec with Matchers {
   "A Borrower" when {
     "request £ 1000 and default data" should {
       "return loan condition" in {
-        request(1000) should equal(Condition(amountRequested = 1000, rate = 7.0, monthlyRepayment = 34.25, totalRepayment = 1233.08))
+        val condition = request(1000)
+        condition should not be empty
+        condition.get should equal(Condition(amountRequested = 1000, rate = 7.0, monthlyRepayment = 34.25, totalRepayment = 1233.08))
       }
     }
   }
