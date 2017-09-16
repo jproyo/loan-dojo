20837b518d0db57cb99fa4e14d823dc3a6cc5d49
Juan Pablo Royo <juanpablo.royo@gmail.com>
2017-09-16 13:40:37 +0200

    advance tdd

--- src/main/scala/edu/jproyo/dojos/loan/Loan.scala
+++ src/main/scala/edu/jproyo/dojos/loan/Loan.scala
@@ -2,6 +2,11 @@ package edu.jproyo.dojos.loan
 
 object Loan {
 
-  def request(amount: Int): Condition = Condition(amount, 7.0, 34.25, 1233.08)
+  /**
+    * Request an amount to be borrowed by one or more lenders at the lowest rate posible
+    * @param amount
+    * @return the condition on which the
+    */
+  def request(amount: Int): Option[Condition] = Condition(amount, 7.0, 34.25, 1233.08)
 
 }
